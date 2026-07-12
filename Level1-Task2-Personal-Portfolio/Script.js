49cdc0a51a5970e919d417e1ee32982b844e8997
dosapallisravani <dosapallisravani87@gmail.com>
2026-07-12 10:19:30 +0530

    Update Script.js

--- Level1-Task2-Personal-Portfolio/Script.js
+++ Level1-Task2-Personal-Portfolio/Script.js
@@ -175,3 +175,62 @@ topBtn.addEventListener("click", () => {
     });
 
 });
+/*==============================
+TYPING ANIMATION
+==============================*/
+
+const words = [
+
+"Frontend Developer",
+
+"Web Designer",
+
+"UI Designer",
+
+"CSE Student"
+
+];
+
+let wordIndex = 0;
+let charIndex = 0;
+let isDeleting = false;
+
+const typing = document.getElementById("typing");
+
+function typeEffect(){
+
+    const currentWord = words[wordIndex];
+
+    if(!isDeleting){
+
+        typing.textContent = currentWord.substring(0, charIndex++);
+    }
+
+    else{
+
+        typing.textContent = currentWord.substring(0, charIndex--);
+    }
+
+    if(!isDeleting && charIndex === currentWord.length + 1){
+
+        isDeleting = true;
+
+        setTimeout(typeEffect,1200);
+
+        return;
+
+    }
+
+    if(isDeleting && charIndex === 0){
+
+        isDeleting = false;
+
+        wordIndex = (wordIndex + 1) % words.length;
+
+    }
+
+    setTimeout(typeEffect, isDeleting ? 60 : 120);
+
+}
+
+typeEffect();
