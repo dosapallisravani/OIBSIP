8a22efdd02bba951a5f7dcda3be35f228a26f912
dosapallisravani <dosapallisravani87@gmail.com>
2026-07-20 10:55:22 +0530

    Update script.js

--- Level2-Task1-premium-calculator/script.js
+++ Level2-Task1-premium-calculator/script.js
@@ -1,6 +1,3 @@
-// =========================
-// NOVACALC SCRIPT
-// =========================
 
 const display = document.getElementById("display");
 
@@ -12,8 +9,8 @@ const deleteBtn = document.querySelector(".delete");
 const equalBtn = document.querySelector(".equal");
 
 
-let firstNumber = null;
-let operator = null;
+let firstNumber = "";
+let operator = "";
 let resetDisplay = false;
 
 
@@ -24,10 +21,8 @@ numbers.forEach(btn => {
     btn.addEventListener("click", () => {
 
         if(resetDisplay){
-
             display.value = "";
             resetDisplay = false;
-
         }
 
         display.value += btn.dataset.value;
@@ -37,16 +32,14 @@ numbers.forEach(btn => {
 });
 
 
-
 // Operators
-operators.forEach(button => {
 
-    button.addEventListener("click", () => {
+operators.forEach(btn => {
 
-        if (display.value === "") return;
+    btn.addEventListener("click", () => {
 
         firstNumber = Number(display.value);
-        operator = button.dataset.value;
+        operator = btn.dataset.value;
 
         resetDisplay = true;
 
@@ -57,84 +50,54 @@ operators.forEach(button => {
 
 // Equal
 
-equalBtn.addEventListener("click", () => {
-
-
-    if(firstNumber === null || operator === null || display.value === "")
-    {
-        return;
-    }
-
+equalBtn.addEventListener("click",()=>{
 
     let secondNumber = Number(display.value);
 
     let result;
 
 
-
-   switch (operator) {
-
-    case "+":
+    if(operator === "+"){
         result = firstNumber + secondNumber;
-        break;
+    }
 
-    case "-":
+    else if(operator === "-"){
         result = firstNumber - secondNumber;
-        break;
+    }
 
-    case "*":
+    else if(operator === "*"){
         result = firstNumber * secondNumber;
-        break;
+    }
 
-    case "/":
+    else if(operator === "/"){
         result = secondNumber === 0 ? "Error" : firstNumber / secondNumber;
-        break;
+    }
 
-    case "%":
+    else if(operator === "%"){
         result = firstNumber % secondNumber;
-        break;
+    }
 
-}
 
     display.value = result;
 
-
-    firstNumber = null;
-
-    operator = null;
-
-    resetDisplay = true;
-
-
 });
-
 
 
 // Clear
 
 clearBtn.addEventListener("click",()=>{
 
-
-    display.value = "";
-
-    firstNumber = null;
-
-    operator = null;
-
-    resetDisplay = false;
-
+    display.value="";
+    firstNumber="";
+    operator="";
 
 });
-
-
 
 
 // Delete
 
 deleteBtn.addEventListener("click",()=>{
 
-
     display.value = display.value.slice(0,-1);
-
 
 });
